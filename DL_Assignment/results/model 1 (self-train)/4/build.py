d10616aaddf5922dcf45a47599c8d22b7ad8d81a
FLaries <teddytsen@gmail.com>
2020-06-09 22:55:23 +0800

    First set of results

---
+++ DL_Assignment/results/model 1 (self-train)/4/build.py
@@ -0,0 +1,45 @@
+# Setting generators
+img_size = 150
+train_datagen = ImageDataGenerator(rescale=1.0/255)
+test_datagen = ImageDataGenerator(rescale=1.0/255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(img_size, img_size),
+        batch_size=20,
+        class_mode='categorical',
+        shuffle=True)
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(img_size, img_size),
+        batch_size=20,
+        class_mode='categorical')
+
+# Build the Model
+model1 = models.Sequential()
+model1.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(img_size, img_size, 3)))
+model1.add(layers.MaxPooling2D((2, 2)))
+model1.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model1.add(layers.MaxPooling2D((2, 2)))
+model1.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model1.add(layers.MaxPooling2D((2, 2)))
+model1.add(layers.Flatten())
+model1.add(layers.Dense(256, activation='relu'))
+model1.add(layers.Dense(256, activation='relu'))
+model1.add(layers.Dense(10, activation='softmax'))
+
+model1.summary()
+
+# Train the Model
+model1.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(learning_rate=0.0005),
+              metrics=['acc'])
+
+history1 = model1.fit_generator(
+      train_generator,
+      steps_per_epoch=375,
+      epochs=20,
+      validation_data=validation_generator,
+      validation_steps=100)
---
+++ DL_Assignment/results/model 2 (pretrained)/2/build.py
@@ -0,0 +1,58 @@
+# Setting up the conv base
+img_size = 150
+
+conv_base = ResNet50(weights='imagenet',
+                  include_top=False,
+                  input_shape=(img_size, img_size, 3))
+conv_base.trainable = True
+set_trainable = False
+for layer in conv_base.layers:
+    if layer.name == 'conv5_block1_1_conv':
+        set_trainable = True # after black5_conv1, set_trainable becomes True
+    if set_trainable:
+        layer.trainable = True
+    else:
+        layer.trainable = False
+
+# Preparing the data generators
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      horizontal_flip=True,
+      rotation_range=10,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      fill_mode='nearest')
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(img_size, img_size),
+        batch_size=20)
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(img_size, img_size),
+        batch_size=20)
+
+# Build the Model
+model2 = models.Sequential()
+model2.add(conv_base)
+model2.add(layers.Flatten())
+model2.add(layers.Dense(256, activation='relu'))
+model2.add(layers.Dense(10, activation='softmax'))
+model2.summary()
+
+# Train the Model
+model2.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(lr=2e-5),
+              metrics=['acc'])
+
+history2 = model2.fit_generator(
+      train_generator,
+      steps_per_epoch=375,
+      epochs=20,
+      validation_data=validation_generator,
+      validation_steps=100,
+      verbose=1)
