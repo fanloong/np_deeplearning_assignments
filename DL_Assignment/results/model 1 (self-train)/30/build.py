79d30ba72fe89b88981602889dbc8e7811491a18
FLaries <teddytsen@gmail.com>
2020-06-11 18:19:27 +0800

    Result update

---
+++ DL_Assignment/results/model 1 (self-train)/30/build.py
@@ -0,0 +1,45 @@
+# Setting generators
+img_size = 150
+
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      horizontal_flip=True,
+      rotation_range=20)
+test_datagen = ImageDataGenerator(rescale=1.0/255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(img_size, img_size),
+        batch_size=20)
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(img_size, img_size),
+        batch_size=20)
+
+# Build the Model
+model1 = models.Sequential()
+model1.add(layers.Conv2D(16, (3, 3), activation='relu',
+                        input_shape=(img_size, img_size, 3)))
+model1.add(layers.MaxPooling2D((2, 2)))
+model1.add(layers.Conv2D(32, (3, 3), activation='relu'))
+model1.add(layers.MaxPooling2D((2, 2)))
+model1.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model1.add(layers.MaxPooling2D((2, 2)))
+model1.add(layers.Flatten())
+model1.add(layers.Dense(256))
+model1.add(layers.Dense(10, activation='softmax'))
+
+model1.summary()
+
+# Train the Model
+model1.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.Adam(learning_rate=0.001),
+              metrics=['acc'])
+
+history1 = model1.fit_generator(
+      train_generator,
+      steps_per_epoch=375,
+      epochs=50,
+      validation_data=validation_generator,
+      validation_steps=100)
